b805d1e818e8b74f210de9c1d23e13ed83e3c5c3
Seventty <elmastigeron2015@gmail.com>
2024-05-22 04:32:01 -0400

    board refactorized

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -5,58 +5,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent  implements OnInit {
-  squares?: any | number[];
-  whoIsNext?: boolean;
+export class BoardComponent implements OnInit {
+  cells?: Array<number> | any;
   winner?: string;
+  nextPlayer?: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
-    this.newGame()
-  }
-
-  newGame(){
-    this.squares = Array(9).fill(null)
-    this.whoIsNext = true;
+  initGame(){
+    this.cells = Array(9).fill(null);
     this.winner = "";
+    this.nextPlayer = true;
   }
 
-  get player(){
-    return this.whoIsNext ? "X" : "O";
+  get getPlayer(){
+    return this.nextPlayer ? "X" : "O"
   }
 
-  makeMove(id: any){
-    if (!this.squares[id]) {
-      this.squares.splice(id, 1, this.player);
-      this.whoIsNext = !this.whoIsNext;
+  selectCell(index: number){
+    if(!this.cells[index] && !this.winner){
+      this.cells.splice(index, 1, this.getPlayer);
+      this.nextPlayer = !this.nextPlayer;
     }
 
     this.winner = this.calculateWinner();
   }
 
-  calculateWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        this.squares[a] &&
-        this.squares[a] === this.squares[b] &&
-        this.squares[a] === this.squares[c]
-      ) {
-        return this.squares[a];
+  calculateWinner(){
+    const winCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], //Horizontal combos
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], //Vertical combos
+      [0, 4, 8], [2, 4, 6] //Diagonal combos
+    ]
+
+    for (let index = 0; index < winCombos.length; index++) {
+      const [a, b, c] = winCombos[index];
+      if(this.cells[a] && this.cells[a] === this.cells[b] && this.cells[a] === this.cells[c]){
+        return this.cells[a]
       }
     }
-    return null;
+
+    const isDraw = this.cells.every((cell: any) => cell);
+    if (isDraw) {
+      return "Draw";
+    }
+  }
+
+  ngOnInit(): void {
+    this.initGame();
   }
 
 }
